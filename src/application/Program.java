c6e7515f3ef3454d9a6676bfd054e3319f8d279f
Felipe Salles <prof.felipe.salles@gmail.com>
2019-07-19 22:17:53 -0300

    first class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
-/**
- *
- * @author Livia
- */
+import boardGame.Position;
+
 public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
     
 }
