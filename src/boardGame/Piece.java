04a980727d60c737cb5b5adc18f71aee6ee73772
Felipe Salles <prof.felipe.salles@gmail.com>
2019-09-14 00:29:18 -0300

    Chess layer and printing the board

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boardGame;
 
-/**
- *
- * @author Livia
- */
 public class Piece {
-    
+
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+	
 }
